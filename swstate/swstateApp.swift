fcfc5335bf624f2b52c0bd66ee388159c4134a0f
Brandon <brandon.se2009@gmail.com>
2021-09-11 13:58:06 -0400

    Initial Commit

---
+++ swstate/swstateApp.swift
@@ -0,0 +1,17 @@
+//
+//  swstateApp.swift
+//  swstate
+//
+//  Created by user203110 on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct swstateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
